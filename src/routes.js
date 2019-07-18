fa828abacb2d806ce4183412d0ee001c623b8280
gustavo <gvmarvelos@gmail.com>
2019-07-17 21:58:53 -0300

    chore: Controler do User

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 import Router from 'express';
 
+import Users from './app/models/Users';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Word' }));
+routes.get('/', async (req, res) => {
+  const user = await Users.create({
+    name: 'Gustavo Arvelos',
+    email: 'gvmarvelos@gmail.com',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
