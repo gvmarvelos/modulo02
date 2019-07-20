e95fa7b40749cefeef6f2ba1b33ca4527df5b03d
gustavo <gvmarvelos@gmail.com>
2019-07-20 18:34:20 -0300

    chore: Validacao dos dados de Entrada

--- src/app/controllers/SessionControler.js
+++ src/app/controllers/SessionControler.js
@@ -1,10 +1,23 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 import Users from '../models/Users';
 
 import authConfig from '../../config/auth';
 
 class SessionContoler {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
     const { email, password } = req.body;
 
     const user = await Users.findOne({ where: { email } });
