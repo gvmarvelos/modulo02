4d22c2a5e97795161bc155c75f327c62b8482584
gustavo <gvmarvelos@gmail.com>
2019-07-23 09:39:32 -0300

    chore: Agendamento de User com Provider e Date

---
+++ src/app/controllers/AppointmentsController.js
@@ -0,0 +1,40 @@
+import * as Yup from 'yup';
+import Appointments from '../models/Appointments';
+import User from '../models/Users';
+
+class AppointmentsController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+    const { provider_id, date } = req.body;
+
+    /**
+     * Verifica se o user é um provedor de serviço
+     */
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'You can only create appointments with providers' });
+    }
+
+    const appointments = await Appointments.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new AppointmentsController();
