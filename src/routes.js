9b7025619f50a435c94ed6db0538bb1d4aa042c7
gustavo <gvmarvelos@gmail.com>
2019-07-24 11:45:46 -0300

    chore: Inclusão da rota de Horarios disponiveis

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentsController from './app/controllers/AppointmentsController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -23,6 +24,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/appointments', AppointmentsController.index);
